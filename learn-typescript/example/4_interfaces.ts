9a627d73578c111b3dc7dd35ee55de05ce2d324b
hanbeulYou <hanbeul.you@gmail.com>
2023-02-26 23:31:15 +0900

    interface 활용

--- learn-typescript/example/4_interfaces.ts
+++ learn-typescript/example/4_interfaces.ts
@@ -14,21 +14,33 @@ function getUser(user: User) {
 getUser(seho);
 
 // 함수의 전체 타입에 사용하는 경우
-// interface SumFunction {
-//   (a: number, b: number): number;
-// }
-// let sum: SumFunction;
-// sum = function (num1: number, num2: string): number {
-//   return num1 + num2;
-// };
+// 약속을 위해 사용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+let sum: SumFunction;
+sum = function (num1: number, num2: number): number {
+  return num1 + num2;
+};
 
 // 배열의 인덱싱에 사용하는 경우
-// interface StringArray {
-//   [index: number]: string;
-// }
-// let arr: StringArray;
-// arr[0] = 'hi';
+interface StringArray {
+  [index: number]: string;
+}
+let arr: StringArray = [];
+arr[0] = 'hi';
 // arr[1] = 10;
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+  [key: string]: RegExp;
+}
+
+let obj: StringRegexDictionary = {
+  // something: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+}
 
 // 인터페이스 확장
 interface Person {
@@ -38,4 +50,4 @@ interface Person {
 interface Developer extends Person {
   language: string;
 }
-const joo: Developer = { name: 'joo', age: 20, language: 'ts' };
+const hayou: Developer = { name: 'hayou', age: 28, language: 'ts' };
