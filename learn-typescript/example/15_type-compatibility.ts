1f1d20b05b7b55d81ccf8eaa86f828bf36561e63
hanbeulYou <hanbeul.you@gmail.com>
2023-03-04 22:44:21 +0900

    타입 호환 예제 학습

--- learn-typescript/example/15_type-compatibility.ts
+++ learn-typescript/example/15_type-compatibility.ts
@@ -1,3 +1,4 @@
+// 인터페이스
 interface ThisDeveloper {
   name: string;
   skill: string;
@@ -7,9 +8,36 @@ interface ThisPerson {
   name: string;
 }
 
-
 let myDeveloper: ThisDeveloper;
 let myPerson: ThisPerson;
-
 myPerson = myDeveloper;
-// myDeveloper = myPerson;
+myDeveloper = myPerson;
+
+// 함수
+let add = function(a: number): number {
+  return a;
+}
+let sum = function(a: number, b: number): number {
+  return a+b;
+}
+
+sum = add;
+// add = sum;
+
+// 제네릭
+interface Empty<T> {
+  // ..
+}
+
+let empty1: Empty<string>;
+let empty2: Empty<string>;
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> {
+  data: T;
+}
+let notEmpty1: NotEmpty<string>;
+let notEmpty2: NotEmpty<number>;
+notEmpty1 = notEmpty2;
+notEmpty2 = notEmpty1;
